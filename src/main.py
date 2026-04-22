78c21b961136697dee8156543f06a4f8f07a70b6
Thomas <thomasmagalhaes935@gmail.comm>
2026-04-22 18:01:51 -0300

    Alteraçao na forma de calcular a umidade para que a funçao de regar a planta consiga funcionar, tambem implementação de uma logica para a planta ir secando com o tempo

--- src/main.py
+++ src/main.py
@@ -1,22 +1,30 @@
 from machine import Pin, ADC, PWM
 import time 
 
-
 potenciometro = ADC(Pin(34))
 potenciometro.atten(ADC.ATTN_11DB)
 
 botao_regar = Pin(33, Pin.IN, Pin.PULL_UP)
-valor_regar = 0
 estado_anterior_botao = 1
 
-def verificar_botao(valor_regar, estado_anterior_botao):
-    estado_atual = botao_regar.value()
-    if estado_anterior_botao == 1 and estado_atual == 0:
-        valor_regar + 500
-        print("Regando a planta!")
-        
-    return valor_regar, estado_atual
+umidade_atual = potenciometro.read() 
 
+
+def verificar_botao(estado_anterior):
+    estado_atual = botao_regar.value()
+    adicionar_agua = 0
+
+    if estado_anterior == 1 and estado_atual == 0:
+        adicionar_agua = 500
+        print("Regando a planta!")
+
+    return adicionar_agua, estado_atual
+
+
+def calcular_secagem(clima):
+    base = 30
+    variacao = int((4095 - clima) / 100)
+    return base + variacao
 
 
 pwm_r = PWM(Pin(4))
@@ -27,18 +35,10 @@ pwm_r.freq(1000)
 pwm_g.freq(1000)
 pwm_b.freq(1000)
 
-cores = [
-       (100, 0, 0),   #vermelho
-       (100, 50, 0),  #laranja
-       (0, 100, 0),   #verde 
-       (0, 0, 100),   #azul
-       (100, 0, 100) #roxo
-]
-
 def set_color(r, g, b):
-    pwm_r.duty_u16(int(r*65535/100))
-    pwm_g.duty_u16(int(g*65535/100))
-    pwm_b.duty_u16(int(b*65535/100))
+    pwm_r.duty(int(r * 1023 / 100))
+    pwm_g.duty(int(g * 1023 / 100))
+    pwm_b.duty(int(b * 1023 / 100))
 
 
 def determinar_estado(valor_umidade):
@@ -53,26 +53,38 @@ def determinar_estado(valor_umidade):
     else:
         return "EXCESSO"
     
-def atualizar_led(estado_da_planta):
-    if estado_da_planta == "SECO_CRITICO":
+
+def atualizar_led(estado):
+    if estado == "SECO_CRITICO":
         set_color(100, 0, 0) 
-    elif estado_da_planta == "SECO":
+    elif estado == "SECO":
         set_color(100, 50, 0)
-    elif estado_da_planta == "IDEAL":
+    elif estado == "IDEAL":
         set_color(0, 100, 0)
-    elif estado_da_planta == "UMIDO":
+    elif estado == "UMIDO":
         set_color(0, 0, 100)
     else: 
         set_color(100, 0, 100)
 
 
-while True: 
-    valor_umidade = ler_umidade()
-    estado_da_planta = determinar_estado(valor_umidade)
-    print("UMIDADE: ", valor_umidade)
-    print("ESTADO: ", estado_da_planta)
+while True:
+    agua_adicionada, estado_anterior_botao = verificar_botao(estado_anterior_botao)
+
+    umidade_atual += agua_adicionada
+
+    clima_base = potenciometro.read()
+    umidade_atual -= calcular_secagem(clima_base)
+
+    if umidade_atual < 0:
+        umidade_atual = 0
+    elif umidade_atual > 4095:
+        umidade_atual = 4095
+
+    estado_da_planta = determinar_estado(umidade_atual)
+
+    print(" Umidade:", umidade_atual)
+    print(" Estado:", estado_da_planta)
+
     atualizar_led(estado_da_planta)
+
     time.sleep(1)
-
-
-
