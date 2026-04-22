7ee0127ad43edef8fb46c74bc86571abe465b2fc
Thomas <thomasmagalhaes935@gmail.comm>
2026-04-22 17:31:32 -0300

    Incrementaçao da mecanica para o usuario conseguir regar a planta, inicio de alterações na funçao de ler_umidade

--- src/main.py
+++ src/main.py
@@ -5,6 +5,20 @@ import time
 potenciometro = ADC(Pin(34))
 potenciometro.atten(ADC.ATTN_11DB)
 
+botao_regar = Pin(33, Pin.IN, Pin.PULL_UP)
+valor_regar = 0
+estado_anterior_botao = 1
+
+def verificar_botao(valor_regar, estado_anterior_botao):
+    estado_atual = botao_regar.value()
+    if estado_anterior_botao == 1 and estado_atual == 0:
+        valor_regar + 500
+        print("Regando a planta!")
+        
+    return valor_regar, estado_atual
+
+
+
 pwm_r = PWM(Pin(4))
 pwm_g = PWM(Pin(5))
 pwm_b = PWM(Pin(18))
@@ -25,9 +39,6 @@ def set_color(r, g, b):
     pwm_r.duty_u16(int(r*65535/100))
     pwm_g.duty_u16(int(g*65535/100))
     pwm_b.duty_u16(int(b*65535/100))
-
-def ler_umidade():
-    return potenciometro.read()
 
 
 def determinar_estado(valor_umidade):
@@ -54,6 +65,7 @@ def atualizar_led(estado_da_planta):
     else: 
         set_color(100, 0, 100)
 
+
 while True: 
     valor_umidade = ler_umidade()
     estado_da_planta = determinar_estado(valor_umidade)
@@ -61,5 +73,6 @@ while True:
     print("ESTADO: ", estado_da_planta)
     atualizar_led(estado_da_planta)
     time.sleep(1)
+
 
 
